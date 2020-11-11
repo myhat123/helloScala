d1f9d78d167631932bfb1f8b8edcdaf1acc1a653
myhat123 <605162579@qq.com>
2020-11-11 20:19:42 +0800

    去除多余的空行

--- hello_18/com/mine/hello/Utils.scala
+++ hello_18/com/mine/hello/Utils.scala
@@ -4,10 +4,7 @@ import com.github.nscala_time.time.Imports._
 
 object Utils {
     def mkDateGroup(startDate: String, days: Int): List[List[String]] = {
-
         var groups = List[List[String]]()
-        
-
         val s = DateTimeFormat.forPattern("yyyyMMdd")
 
         var k2 = LocalDate.parse(startDate, s)
@@ -15,15 +12,14 @@ object Utils {
         for (i <- 1 to days) {
             var g = List[String]()
             var k1 = k2 - 1.day
+
             g = g :+ k1.toString("yyyyMMdd")
             g = g :+ k2.toString("yyyyMMdd")
 
             groups = groups :+ g
-
             k2 = k2 - 1.day
-            
         }
-
+        
         return groups
     }
 }
